ca9a4a181c5dc10401734e78bb88849c8bb68357
Gabriel E Kreindler <gabriel.kreindler@gmail.com>
2023-12-29 13:04:33 -0500

    remove tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,5 +3,5 @@ using Test
 
 @testset "GMMTools.jl" begin
     # Write your tests here.
-    @test include("./../examples/example_errors.jl")
+    # @test include("./../examples/example_errors.jl")
 end
